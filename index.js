07c97e8bd696aa2dc915f4f0542b419df7bdef4a
Ramanathan M <ramanathanm@Ramanathans-MacBook-Pro.local>
2021-04-04 14:45:17 +0530

    change nginx start command

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const shell = require("shelljs");
 require("dotenv").config();
-if (
-  shell.exec(`nginx -s reload  -c ${process.env.NGINX_CONFIG_PATH}`).code !== 0
-) {
+if (shell.exec(`nginx  -c ${process.env.NGINX_CONFIG_PATH}`).code !== 0) {
   shell.echo("Nginx failed to start");
   shell.exit(1);
 }
